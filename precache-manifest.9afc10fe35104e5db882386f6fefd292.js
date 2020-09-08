58519a5227643ab2aa6e3f43f9c6c37fe35ee3d5
Din Pl <DinPlo@test.com>
2020-09-08 15:24:26 +0200

    Updates

---
+++ precache-manifest.9afc10fe35104e5db882386f6fefd292.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b18f6c6898225f5fdba5e369140a266",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "e9c97b84de84f0d8d725",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "736c0e5719d74213c575",
+    "url": "/robofriends/static/css/main.c7f80e4d.chunk.css"
+  },
+  {
+    "revision": "e9c97b84de84f0d8d725",
+    "url": "/robofriends/static/js/2.0b5b9267.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.0b5b9267.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "736c0e5719d74213c575",
+    "url": "/robofriends/static/js/main.da573830.chunk.js"
+  },
+  {
+    "revision": "e7d2cbb368e0813da61d",
+    "url": "/robofriends/static/js/runtime-main.74eee7b5.js"
+  }
+]);
